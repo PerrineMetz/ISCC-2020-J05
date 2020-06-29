8e73c76c95a7fd136661331bf0e41424c4b66661
PerrineMetz <67265994+PerrineMetz@users.noreply.github.com>
2020-06-29 16:00:03 +0200

    day 5

--- EX_02/navigation.php
+++ EX_02/navigation.php
@@ -2,4 +2,5 @@
                 <a class="encours" href="vitrine-accueil.php">Accueil</a>
                 <a href="vitrine-contacts.php">Contact</a>
                 <a href="vitrine-programme.php">Programme</a>
+        
 </nav>
